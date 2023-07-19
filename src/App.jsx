1220b3f517b0cc6b3a503a30c8011ed80e3eadab
nassims01 <nassimsalomon01@gmail.com>
2023-07-19 17:41:56 -0300

    fix first click in button toggle theme

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Header from "./components/header/Header";
 import themes from "./Theme/Themes";
 
 function App() {
-  const [theme, setTheme] = useState(localStorage.getItem("valueTheme"));
+  const [theme, setTheme] = useState(localStorage.getItem("valueTheme") || "light");
   return (
       <div className="theme" data-theme={theme}>
         <Header></Header>
